75a9a34830573e7eb894375fcfc1a120c5100296
akboada <crosseyedjonny@yahoo.com>
2015-04-08 23:22:34 -0700

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
-    rimraf = require('gulp-rimraf');
+    minifyCSS = require('gulp-minify-css'),
+    minifyInline = require('gulp-minify-inline'),
+    concatify = require('gulp-concat');
 
 var paths = {
 	scripts: ['./js/**/*.js','./views/js/**/*.js'],
-	content: ['./*.html','./views/**/*.html']
+	content: ['./*.html','./views/**/*.html'],
+    styles: ['./css/**/*.css','./views/css/**/*.css']
 }
 
 gulp.task('scripts', function(){
-     gulp.src(paths.scripts, {base: "./"})
+     gulp.src(paths.scripts, {base: './'})
         .pipe(uglify())
         .pipe(gulp.dest('./build'));
 });
 
 gulp.task('content', function() {
-    return gulp.src(paths.content, {base: "./"})
+    return gulp.src(paths.content, {base: './'})
         .pipe(minifyHTML({
             empty: true,
             quotes: true
         }))
+        .pipe(minifyInline())
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('clean', function(cb) {
-    rimraf('./build', cb);
-});
-
-gulp.task('default', ['clean', 'scripts', 'content']);
+gulp.task('styles', function() {
+    gulp.src(paths.styles, {base:'./'})
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build'));
+})
+gulp.task('default', ['scripts', 'content', 'styles']);
